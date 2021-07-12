de13e4a239070a89eb2089a81d46977fc040b277
Savin Volodya <SavinVolodya001@gmail.com>
2021-07-12 19:04:04 +0300

    feat: implement SessionAccess in data access layer

--- DataAccessLayer/SessionsAccess.js
+++ DataAccessLayer/SessionsAccess.js
@@ -1,17 +1,37 @@
-module.exports = class ServiceAccess{
+const sessionConsts = require('../consts/session')
+const Timer = require('../service_modules/Timer')
+
+module.exports = class SessionAccess{
     constructor(){
         this.sessionsMap = new Map();
+
+        this.deleteSession = this.deleteSession.bind(this);
     }
 
     addSession(sessionID, sessionInstance){
-
+        this.sessionsMap.set(
+            sessionID,
+            {
+                session: sessionInstance,
+                idleTimer: new Timer(()=>this.deleteSession(sessionID), sessionConsts.IDLE_TIME)
+            }
+        );
     }
 
     deleteSession(sessionID){
-
+        this.sessionsMap.get(sessionID).session.destroy();
+        this.sessionsMap.delete(sessionID)
     }
     
     getSession(sessionID){
-        
+        this.sessionsMap.get(sessionID)
+    }
+
+    hasSession(sessionID){
+        return this.sessionsMap.has(sessionID)
+    }
+
+    resetSessionIdleTimer(sessionID){
+        this.sessionsMap.get(sessionID).idleTimer.reset()
     }
 }
---
+++ consts/session.js
@@ -0,0 +1,3 @@
+module.exports = { 
+    IDLE_TIME: 60*1000*15 //[ms]
+};
