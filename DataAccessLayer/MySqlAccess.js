c870531ecaea6e0bcc56faf3904bfefd0d4069e1
Savin Volodya <SavinVolodya001@gmail.com>
2021-07-13 14:19:41 +0300

    feat: implement MySqlAccess according to tests
    Return value has to contain 2 defined props value: password
    and role_code.
    Also added connection config to specify SQL connection
    parameters.

--- DataAccessLayer/MySqlAccess.js
+++ DataAccessLayer/MySqlAccess.js
@@ -1,5 +1,30 @@
-module.exports = class MySqlAccess{
-    getUser(login){
+const mysql = require('mysql2/promise')
+const CONFIG = require('../connectionConfig.json')
 
+module.exports = class MySqlAccess{
+    async _init(){
+        this._connection = await mysql.createConnection({
+            host: CONFIG.dbHost,
+            user: CONFIG.user,
+            database: CONFIG.dbName,
+            password: CONFIG.password
+        })
+        this._isConnectionEstablished = true;
+    }
+
+    async getUser(login){
+        let sqlRequest = 'SELECT password, role_ref as role_code FROM users ' +
+                         'JOIN user_roles ON users.id = user_roles.user_ref '+ 
+                         `WHERE login = '${login}'`;
+
+        if(!this._isConnectionEstablished) await this._init();
+        let [ rows, __ ] = await this._connection.query(sqlRequest); 
+
+        return rows[0];
+    }
+
+    async closeConnection(){
+        await this._connection.end();
+        this._isConnectionEstablished = false;
     }
 } 
