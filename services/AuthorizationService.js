ebd34d90f7af23fe47e68cce6110116700a9e87c
Savin Volodya <SavinVolodya001@gmail.com>
2021-07-16 11:07:49 +0300

    feat: implement AuthorizationService

--- services/AuthorizationService.js
+++ services/AuthorizationService.js
@@ -1,9 +1,46 @@
 const ServiceAccess = require('../DataAccessLayer/SessionsAccess')
 const MySqlAccess = require('../DataAccessLayer/MySqlAccess')
 
-exports = class AuthorizationService{
+module.exports = class AuthorizationService{
     constructor(){
-        this.sessionModelInstance = new ServiceAccess();
-        this.DBmodelInstance = new MySqlAccess();
+        this._sessionModelInstance = new ServiceAccess();
+        this._DBmodelInstance = new MySqlAccess();
+    }
+
+    async authenticateUser(requestData){
+        let response = {
+            isAuthentificated: false,
+            err: null,
+            session: null
+        }
+
+        try{
+            let user = await this._DBmodelInstance.getUser(requestData.login);
+
+            if(user.password !== requestData.password)
+                throw new Error('Authentification failed: invalid login or password');
+            if(user.role_code !== 'ADM')
+                throw new Error('Access denied: admin permissions required')
+
+            this._allow(response)
+        }catch(e){
+            response.err = e;
+            console.log(e);
+        }
+
+        return response;
+    }
+
+    isAuthorized(sessionID){
+        return this._sessionModelInstance.hasSession(sessionID)
+    }
+
+    _allow(authenticationState){
+        let SESSION_ID = require('crypto').randomBytes(64).toString('hex');
+        
+        authenticationState.session = {id: SESSION_ID};
+        authenticationState.isAuthentificated = true;
+
+        this._sessionModelInstance.addSession(SESSION_ID, {...authenticationState.session});
     }
 }
