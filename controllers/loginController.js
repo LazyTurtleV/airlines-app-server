5b9117c2bb73935b2f7d9ad00f7b789a68c54d4d
Savin Volodya <SavinVolodya001@gmail.com>
2021-07-17 12:05:19 +0300

    feat: implement login controller according to test

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,5 +1,35 @@
-module.exports = class LoginController{
-    login(req, res){
-        
+const { IDLE_TIME } = require("../consts/session");
+const AuthorizationService = require("../services/AuthorizationService")
+const cookieParser = require('cookie-parser');
+
+class LoginController{
+    constructor(){
+        this._loginService = new AuthorizationService();
+        this._secretKey = require('crypto').randomBytes(64).toString('HEX');
+    }
+    
+    async login(req, res){
+        cookieParser(this._secretKey);
+
+        let authorizationStatus = await this._loginService.authenticateUser({
+            login: req.body.login,
+            password: req.body.password
+        })
+
+        if(authorizationStatus.isAuthentificated){
+            res.cookie('sid', authorizationStatus.session.id, {
+                MaxAge: IDLE_TIME,
+                signed: true
+            })
+        }
+
+        res.send(authorizationStatus)
+    }
+
+    isUserAuthorized(sessionID){
+        return this._loginService.isAuthorized(sessionID)
     }
 }
+
+const loginControllerInst = new LoginController()
+module.exports = loginControllerInst
