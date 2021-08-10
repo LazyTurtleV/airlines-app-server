de3590eabe838ea7f382a8e1e8076300bcbf6aa8
Savin Volodya <SavinVolodya001@gmail.com>
2021-08-10 14:41:12 +0300

    fix: add CORS policy middleware

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const PORT = 4000;
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 
 //CORS policy
-app.use(function (req, res, next) {
-
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    next();
-});
+app.use(
+    cors({
+        origin: true,
+        credentials: true
+    })
+);
 
 const loginRouter = require('./routes/login');
 
