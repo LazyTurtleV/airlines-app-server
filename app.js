fcf74712ea4556d2d37161ea84a908e841032156
Savin Volodya <SavinVolodya001@gmail.com>
2021-08-10 12:17:36 +0300

    fix: add CORS policy support

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const PORT = 4000;
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 
+//CORS policy
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
 const loginRouter = require('./routes/login');
 
 app.use("/login", loginRouter)
