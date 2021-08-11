ffdaf5687b93af22fef01570ad2a2f5f2fa3cff7
Savin Volodya <SavinVolodya001@gmail.com>
2021-08-11 17:09:10 +0300

    test: add tests for login service encryption

--- tests/services/AuthorizationService.test.js
+++ tests/services/AuthorizationService.test.js
@@ -10,8 +10,29 @@ describe('Authorization service', ()=>{
         serviceInst = new AuthorizationService();
     })
 
+    test('Encryption must generate valid hash', ()=>{
+        let testData = [
+            {
+                input: {login: 'test1', password: 'qwerty123'},
+                validHash: '6ca74ff9de8a53f661f4262558deb641'
+            },
+            {
+                input: {login: 'test2', password: 'qweasdzxc'},
+                validHash: '78d7f0e96a4350a611cea474642d044c'
+            }
+        ];
+
+        for(let testCase of testData){
+            expect(serviceInst._encrypt(testCase.input)).toBe(testCase.validHash)
+        }
+    })
+
     test('If login&password valid and role code is ADM, session must be started', async ()=>{
-        serviceInst._DBmodelInstance.getUser.mockImplementation(async ()=>({password: 'User', role_code: 'ADM'}));
+        serviceInst._DBmodelInstance.getUser.mockImplementation(async login=>{
+            if(login === 'User'){
+                return {password: serviceInst._encrypt('User'), role_code: 'ADM'}
+            }
+        });
         
         let response = await serviceInst.authenticateUser({
             login:  'User',
@@ -19,6 +40,33 @@ describe('Authorization service', ()=>{
         });
 
         expect(response).toBeDefined();
-        expect(serviceInst._sessionModelInstance.hasSession(response.session.id)).toBe(true);
+        expect(serviceInst._sessionModelInstance.hasSession(response.session?.id)).toBe(true);
     })
+    
+    test('If login or password ivalid, authentefication shouldn`t pass', async ()=>{
+        serviceInst._DBmodelInstance.getUser.mockImplementation(async login=>{
+            if(login === 'SomeUser'){
+                return {password: serviceInst._encrypt({password: 'SomePassword', login: "SomeUser"}), role_code: 'ADM'}
+            }
+        });
+
+        let testData = [
+            {
+                login:  'User',
+                password: 'User'
+            },
+            {
+                login:  'SomeUser',
+                password: 'User'
+            }
+        ];
+
+        for(let testCase of testData){
+            let response = await serviceInst.authenticateUser(testCase);
+
+            expect(response.isAuthentificated).toBe(false)
+            expect(serviceInst._sessionModelInstance.hasSession(response.session?.id)).toBe(false);
+        }
+    })
+
 })
